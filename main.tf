01cb5b94dd4c0b49ae0bcde4f697e6fdc17fcb39
Neymarsaini <harshad.s@utthunga.com>
2021-10-22 16:32:18 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,46 @@
+provider "azurerm" {
+    version = "2.5.0"
+    features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blob"
+        storage_account_name = "terraformaccountharshad"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+variable "imagebuild" {
+  type        = string
+  description = "Latest Image Build"
+}
+
+
+resource "azurerm_resource_group" "tf_test" {
+  name = "tfmainrg"
+  location = "Australia East"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  name                      = "weatherapi"
+  location                  = azurerm_resource_group.tf_test.location
+  resource_group_name       = azurerm_resource_group.tf_test.name
+
+  ip_address_type     = "public"
+  dns_name_label      = "neymarsainiazure"
+  os_type             = "Linux"
+
+  container {
+      name            = "weatherapi"
+      image           = "harshadsaini/weatherapi:${var.imagebuild}"
+        cpu             = "1"
+        memory          = "1"
+
+        ports {
+            port        = 80
+            protocol    = "TCP"
+        }
+  }
+}
